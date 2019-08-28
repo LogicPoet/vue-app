ebee473a357aac89c9a04f7d6408c2429d73a3b9
LogicPoet <2659712343@qq.com>
2019-08-28 21:44:12 +0800

    6宫格及组件切换动画

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,10 @@
     <mt-header fixed title="练习·Vue项目"></mt-header>
 
     <!-- 中间的 路由 router-view 区域 -->
-    <router-view></router-view>
-
+    <transition mode="out-in">
+     <router-view></router-view>
+    </transition>
+   
     <!-- 底部 Tabbar 区域 -->
     <nav class="mui-bar mui-bar-tab">
       <router-link class="mui-tab-item" to="/home">
@@ -41,6 +43,22 @@
 // 防止顶端固定标题挡住中间的区域
 .app-container {
   padding-top: 40px;
-  overflow-x: hidden;
+  overflow-x: hidden;//隐藏组件切换时出现的横向滚动条
+}
+
+// 组件滑动样式
+.v-enter{//从右边进入
+  opacity: 0;
+  transform: translateX(100%);
+}
+.v-leave-to{//从左边离开
+  opacity: 0;
+  transform: translateX(-100%);
+  position: absolute;//绝对布局
+}
+
+.v-enter-active,
+.v-leave-active{
+  transition: all 0.5s ease;
 }
 </style>
--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -1,10 +1,51 @@
 <template>
   <div>
+    <!-- 轮播 -->
     <mt-swipe :auto="4000">
       <mt-swipe-item v-for="(item, index) in lunbotuList" :key="index">
-        <img :src="item.img" alt="">
+        <img :src="item.img" alt />
       </mt-swipe-item>
     </mt-swipe>
+
+    <!-- 6宫格 -->
+    <ul class="mui-table-view mui-grid-view mui-grid-9">
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu1.png" alt />
+          <div class="mui-media-body">新闻资讯</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu2.png" alt />
+          <div class="mui-media-body">图片分享</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu3.png" alt />
+          <div class="mui-media-body">商品购买</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu4.png" alt />
+          <div class="mui-media-body">留言反馈</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu5.png" alt />
+          <div class="mui-media-body">视频专区</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../images/menu6.png" alt />
+          <div class="mui-media-body">联系我们</div>
+        </a>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -12,38 +53,58 @@
 // 导入弹窗组件
 import { Toast } from "mint-ui";
 
-export default{
+export default {
   data() {
     return {
       lunbotuList: [] // 保存轮播图的数组
-    }
+    };
   },
   created() {
-    this.getLunBoTu()
+    this.getLunBoTu();
   },
   methods: {
-    getLunBoTu(){
-      this.$http.get('http://www.liulongbin.top:3005/api/getlunbo').then(result=>{
-        if(result.body.status===0){
-          // 获取成功
-          this.lunbotuList=result.body.message
-        }else{
-          // 获取失败
-          Toast("加载轮播图失败。。。")
-        }
-      })
+    getLunBoTu() {
+      this.$http
+        .get("http://www.liulongbin.top:3005/api/getlunbo")
+        .then(result => {
+          if (result.body.status === 0) {
+            // 获取成功
+            this.lunbotuList = result.body.message;
+          } else {
+            // 获取失败
+            Toast("加载轮播图失败。。。");
+          }
+        });
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
 // 轮播样式
-.mint-swipe{
+.mint-swipe {
   height: 200px;
-  img{
+  img {
     width: 100%;
-    height:100%;
+    height: 100%;
   }
+}
+
+// 6宫格样式
+.mui-grid-view.mui-grid-9 {
+  background-color: #fff;
+  border: none;
+  img {
+    width: 60px;
+    height: 60px;
+  }
+
+  .mui-media-body{
+    font-size: 13px;
+  }
+}
+
+.mui-grid-view.mui-grid-9 .mui-table-view-cell {
+  border: none;
 }
 </style>
