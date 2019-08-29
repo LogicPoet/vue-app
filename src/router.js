cddf66617bf47f9448579480c3c4fd940fe3d568
LogicPoet <2659712343@qq.com>
2019-08-29 20:58:17 +0800

    添加图片评论组件的顶部滑动导航栏

--- src/router.js
+++ src/router.js
@@ -8,6 +8,7 @@ import SearchContainer from './components/tabbar/SearchContainer.vue'
 import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
 import NewsList from './components/news/NewsList.vue'
 import NewsInfo from './components/news/NewsInfo.vue'
+import PhotoList from './components/photos/photoList.vue'
 
 // 3. 创建路由对象
 var router = new VueRouter({
@@ -41,6 +42,11 @@ var router = new VueRouter({
     {
       path: '/news/newsInfo/:id',
       component: NewsInfo
+    },
+    // 新闻详情
+    {
+      path: '/photos/photoList',
+      component: PhotoList
     },
   ],
   linkActiveClass: 'mui-active' // 覆盖默认的路由高亮的类，默认的类叫做 router-link-active
